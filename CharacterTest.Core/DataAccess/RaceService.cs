770e02132bb44b46c951d594a9fd1603310f6481
jsimpson <jsimpson@unionhomemortgage.com>
2014-12-10 14:19:44 -0500

    12-10-14 Lunch Changes

--- CharacterTest.Core/DataAccess/RaceService.cs
+++ CharacterTest.Core/DataAccess/RaceService.cs
@@ -4,11 +4,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace CharacterTest.Core.DataAccess
 {
     public class RaceService
     {
+        private readonly DbContext _context;
         private readonly IGenericRepository<RaceObj> _raceRepo;
+
+
+
+        public RaceService() 
+        {
+            _raceRepo = new GenericRepository<RaceObj>(_context);
+        }
     }
 }
